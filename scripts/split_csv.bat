001880cda41eee575afcf253b68a635556029014
louiscklaw <logickee@gmail.com>
2022-02-09 16:36:46 +0800

    update,

--- scripts/split_csv.bat
+++ scripts/split_csv.bat
@@ -1,4 +1,4 @@
 pushd scripts
   rm -rf x*
-  split C:\Users\logic\_TODO\lcsc2kicad\scripts\lcsc_full_list.csv -l 29999
+  split C:\Users\logic\_TODO\lcsc2kicad\scripts\lcsc_full_list.csv -l 9999
 popd
--- scripts/test.bat
+++ scripts/test.bat
@@ -5,12 +5,36 @@ pipenv install -r requirements.txt
 start pipenv run python ./fetch_component.py scripts/xaa
 start pipenv run python ./fetch_component.py scripts/xab
 start pipenv run python ./fetch_component.py scripts/xac
-start pipenv run python ./fetch_component.py scripts/xad
-start pipenv run python ./fetch_component.py scripts/xae
-start pipenv run python ./fetch_component.py scripts/xaf
-start pipenv run python ./fetch_component.py scripts/xag
-start pipenv run python ./fetch_component.py scripts/xah
-start pipenv run python ./fetch_component.py scripts/xai
-start pipenv run python ./fetch_component.py scripts/xaj
-start pipenv run python ./fetch_component.py scripts/xak
-start pipenv run python ./fetch_component.py scripts/xal
+@REM start pipenv run python ./fetch_component.py scripts/xad
+@REM start pipenv run python ./fetch_component.py scripts/xae
+@REM start pipenv run python ./fetch_component.py scripts/xaf
+@REM start pipenv run python ./fetch_component.py scripts/xag
+@REM start pipenv run python ./fetch_component.py scripts/xah
+@REM start pipenv run python ./fetch_component.py scripts/xai
+@REM start pipenv run python ./fetch_component.py scripts/xaj
+@REM start pipenv run python ./fetch_component.py scripts/xak
+@REM start pipenv run python ./fetch_component.py scripts/xal
+@REM start pipenv run python ./fetch_component.py scripts/xam
+@REM start pipenv run python ./fetch_component.py scripts/xan
+@REM start pipenv run python ./fetch_component.py scripts/xao
+@REM start pipenv run python ./fetch_component.py scripts/xap
+@REM start pipenv run python ./fetch_component.py scripts/xaq
+@REM start pipenv run python ./fetch_component.py scripts/xar
+@REM start pipenv run python ./fetch_component.py scripts/xas
+@REM start pipenv run python ./fetch_component.py scripts/xat
+@REM start pipenv run python ./fetch_component.py scripts/xau
+@REM start pipenv run python ./fetch_component.py scripts/xav
+@REM start pipenv run python ./fetch_component.py scripts/xaw
+@REM start pipenv run python ./fetch_component.py scripts/xax
+@REM start pipenv run python ./fetch_component.py scripts/xay
+@REM start pipenv run python ./fetch_component.py scripts/xaz
+@REM start pipenv run python ./fetch_component.py scripts/xba
+@REM start pipenv run python ./fetch_component.py scripts/xbb
+@REM start pipenv run python ./fetch_component.py scripts/xbc
+@REM start pipenv run python ./fetch_component.py scripts/xbd
+@REM start pipenv run python ./fetch_component.py scripts/xbe
+@REM start pipenv run python ./fetch_component.py scripts/xbf
+@REM start pipenv run python ./fetch_component.py scripts/xbg
+@REM start pipenv run python ./fetch_component.py scripts/xbh
+@REM start pipenv run python ./fetch_component.py scripts/xbi
+@REM start pipenv run python ./fetch_component.py scripts/xbj
